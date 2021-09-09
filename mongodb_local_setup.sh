1ddf1b603809f5d970730524c1f7c57935cd21fa
smchitre <smchitre@gmail.com>
2021-09-09 07:57:15 +0000

    20210909-smchitre-Fix sudo permission issue for mongodb repo config file

--- mongodb_local_setup.sh
+++ mongodb_local_setup.sh
@@ -3,14 +3,14 @@
 echo "Installing python3..."
 sudo yum install python3
 
-sudo cat << EOF > /etc/yum.repos.d/mongodb-org-5.0.repo
+sudo sh -c 'cat << EOF > /etc/yum.repos.d/mongodb-org-5.0.repo
 [mongodb-org-5.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/5.0/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
-EOF
+EOF'
 
 echo "Installing mongodb..."
 
